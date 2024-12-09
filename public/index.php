19c3874c8b35742f22164d76b0965e4643445886
Slava <slava@MacBook-Slava.local>
2024-12-09 15:56:07 +0300

    first commit

--- public/index.php
+++ public/index.php
@@ -6,12 +6,14 @@ use Slim\Factory\AppFactory;
 use DI\Container;
 
 $container = new Container();
-$container->set('renderer', function () {
+$container->set('render', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila','mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -20,9 +22,23 @@ $app->get('/', function ($request, $response) {
     // return $response->write('Welcome to Slim!');
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/users', function ($request, $response) use ($users) {
+    $includes = $request->getQueryParam('includes'); // Получение параметра из запроса
+    $filteredStrings = [];
+    
+    foreach ($users as $user) {
+        if (str_contains($user, $includes)) { // Проверяем, содержится ли строка
+            $filteredStrings[] = $user; // Добавляем в результат
+        }
+    }
+    
+    $params = [
+        'users' => $filteredStrings // Правильный синтаксис массива
+    ];
+    
+    return $this->get('render')->render($response, 'users/index.phtml', $params); // Возвращаем отрендеренный ответ
 });
+
 
 $app->post('/users', function($request, $response){
     return $response->withStatus(302);
@@ -38,8 +54,9 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
     // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
     // $this в Slim это контейнер зависимостей
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+    return $this->get('render')->render($response, 'users/show.phtml', $params);
 });
+
 
 
 $app->run();
